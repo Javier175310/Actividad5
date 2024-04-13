d0dfcec7e5834ddf7feb8ce165be11457885f1bb
Javier175310 <156013441+Javier175310@users.noreply.github.com>
2024-04-13 14:22:47 -0600

    Update Manager.java

--- Activida7/Manager.java
+++ Activida7/Manager.java
@@ -1,20 +1,25 @@
 package lista;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class Manager {
-    public static void saveNumbers(List<Integer> generaFibonacci) {
+    public static void saveNumbers(List<Integer> numbers, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (Integer number : numbers) {
+                writer.write(number + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    public static void saveEvenNumbers(List<Integer> evenNumbers) {
-    }
-    
-    public static void saveOddNumbers(List<Integer> oddNumbers) {
-    }
-    
+
     public static void ejecutar(List<Integer> generaFibonacci) {
-        saveNumbers(generaFibonacci);
-        saveEvenNumbers(Splitter.evenNum(generaFibonacci));
-        saveOddNumbers(Splitter.oddNum(generaFibonacci));
+        saveNumbers(generaFibonacci, "fibonacci.txt");
+        List<Integer> evenNumbers = Splitter.evenNum(generaFibonacci);
+        List<Integer> oddNumbers = Splitter.oddNum(generaFibonacci);
+        saveNumbers(evenNumbers, "evenNumbers.txt");
+        saveNumbers(oddNumbers, "oddNumbers.txt");
     }
 }
